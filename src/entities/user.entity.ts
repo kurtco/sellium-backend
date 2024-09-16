c352e196ee8b1184e930c7c8f3c0c3e95468eaa5
John Fuentes <kurtco.5@gmail.com>
2024-09-16 17:14:33 -0500

    improving entity and the service

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -12,50 +12,51 @@ export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ nullable: true })
   recruiterName: string;
 
   @Column()
   leaderName: string;
 
-  @Column()
-  startDate: Date;
+  @Column({ nullable: true })
+  userName: string;
 
-  @Column()
-  birthDate: Date;
-
-  @Column()
-  phone: string;
-
-  @Column()
-  email: string;
-
-  @Column()
-  homeAddress: string;
-
-  @Column()
-  businessAddress: string;
-
-  @Column()
-  spouseName: string;
-
-  @Column()
-  username: string;
-
-  @Column()
+  @Column({ nullable: true })
   position: string;
 
-  // recruiterCode field, required and unique
-  @Column({ unique: true })
+  // recruiterCode field, required but not unique
+  @Column({ nullable: true, type: "varchar" })
   recruiterCode: string;
 
   // userCode field, required and unique
-  @Column({ unique: true })
+  @Column({ nullable: true, type: "varchar", unique: true })
   userCode: string;
+
+  @Column({ nullable: true })
+  startDate: Date;
+
+  @Column({ nullable: true })
+  birthDate: Date;
+
+  @Column({ nullable: true })
+  phone: string;
+
+  @Column({ nullable: true })
+  email: string;
+
+  @Column({ nullable: true })
+  homeAddress: string;
+
+  @Column({ nullable: true })
+  businessAddress: string;
+
+  @Column({ nullable: true })
+  spouseName: string;
 
   // Recursive relationship with the same User entity
   @ManyToOne(() => User, (user) => user.recruits)
-  @JoinColumn({ name: "recruiterCode", referencedColumnName: "userCode" })
+  //  @JoinColumn({ name: "recruiterCode", referencedColumnName: "userCode" })
+  @JoinColumn({ name: "recruiterCode", referencedColumnName: "userCode" }) // Usa userCode como referencia
   recruiter: User; // this column represents the recluiter  of  a user (userCode),
 
   // Inverse relationship for the recruits
--- src/ocr/ocr.service.ts
+++ src/ocr/ocr.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
 import {
   DataFromImage,
   HttpSuccessResponse,
@@ -7,44 +6,80 @@ import {
 } from "src/interfaces/interfaces";
 import {
   client,
-  projectId,
-  location,
-  processorId,
   getProcessorName,
+  ProcessingBase64,
 } from "src/config/constants";
+import { handleError } from "src/utils/HandleError";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "src/entities/user.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class OcrService {
-  constructor(private configService: ConfigService) {}
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User> // Inyecta el repositorio
+  ) {}
   async processImage(imageBase64: string): Promise<ProcessImageResponse> {
-    const name = getProcessorName();
-    const cleanBase64 = imageBase64.replace(
-      /^data:image\/(png|jpeg);base64,/,
-      ""
-    );
+    try {
+      const extractedData: DataFromImage =
+        await this.processImageWithDocumentAI(imageBase64);
 
+      // Verify if the  recruiter (reclutador) already exist
+      let recruiter = await this.userRepository.findOne({
+        where: { userCode: extractedData.recruiterCode },
+      });
+
+      // If the recruiter (reclutador) does not exist, create a new record for him/her
+      if (!recruiter) {
+        recruiter = this.userRepository.create({
+          userCode: String(extractedData.recruiterCode),
+          userName: extractedData.recruiterName,
+          leaderName: extractedData.leaderName,
+        });
+        await this.userRepository.save(recruiter);
+      }
+
+      // Check if the recruit (usuario reclutado) already exists
+      let recruit = await this.userRepository.findOne({
+        where: { userCode: extractedData.userCode },
+      });
+
+      if (!recruit) {
+        // creating the new recruit
+        const user = this.userRepository.create(extractedData);
+        await this.userRepository.save(user);
+      }
+
+      return { data: extractedData } as HttpSuccessResponse<DataFromImage>;
+    } catch (error) {
+      return handleError(error, "Failed to process image");
+    }
+  }
+
+  private async processImageWithDocumentAI(
+    base64image: string
+  ): Promise<DataFromImage> {
+    const cleanBase64 = ProcessingBase64(base64image);
+    const name = getProcessorName();
     const request = {
       name,
       rawDocument: {
-        content: cleanBase64, // Usar el base64 limpio
-        mimeType: "image/png", // O el tipo de imagen adecuado
+        content: cleanBase64,
+        mimeType: "image/png",
       },
     };
-
     const [result] = await client.processDocument(request);
 
     const entities = result.document.entities;
 
     const extractedData: DataFromImage = {
-      recruiter: "",
-      leader: "",
-      startDate: "",
-      birthDate: "",
+      recruiterName: "",
+      leaderName: "",
       phone: "",
       email: "",
       homeAddress: "",
       businessAddress: "",
-      spouse: "",
+      spouseName: "",
       userName: "",
       position: "",
       recruiterCode: "",
@@ -64,13 +99,13 @@ export class OcrService {
           );
           break;
         case "recruiter":
-          extractedData.recruiter = entity.mentionText;
+          extractedData.recruiterName = entity.mentionText;
           break;
         case "birthDate":
-          extractedData.birthDate = entity.mentionText;
+          extractedData.birthDate = new Date(entity.mentionText);
           break;
         case "leader":
-          extractedData.leader = entity.mentionText;
+          extractedData.leaderName = entity.mentionText;
           break;
         case "email":
           extractedData.email = entity.mentionText;
@@ -79,7 +114,7 @@ export class OcrService {
           extractedData.homeAddress = entity.mentionText?.replace(/\n/g, " ");
           break;
         case "spouse":
-          extractedData.spouse = entity.mentionText;
+          extractedData.spouseName = entity.mentionText;
           break;
         case "userName":
           extractedData.userName = entity.mentionText;
@@ -88,19 +123,18 @@ export class OcrService {
           extractedData.position = entity.mentionText;
           break;
         case "recruiterCode":
-          extractedData.recruiterCode = entity.mentionText;
+          extractedData.recruiterCode = String(entity.mentionText);
           break;
         case "startDate":
-          extractedData.startDate = entity.mentionText;
+          extractedData.startDate = new Date(entity.mentionText);
           break;
         case "userCode":
-          extractedData.userCode = entity.mentionText;
+          extractedData.userCode = String(entity.mentionText);
           break;
         default:
           break;
       }
     });
-
-    return { data: extractedData } as HttpSuccessResponse;
+    return extractedData;
   }
 }
